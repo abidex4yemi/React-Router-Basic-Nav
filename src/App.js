8a78488126b4a880cb43a8be3cf8ea8dda81caa9
abidex4yemi <abidex4yemi@gmail.com>
2019-06-10 15:39:40 +0100

    Completed mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+const routeDetails = [
+	{
+		id: 1,
+		path: '/',
+		ComponentToRender: Home
+	},
+	{
+		id: 2,
+		path: '/about',
+		ComponentToRender: About
+	},
+	{
+		id: 3,
+		path: '/contact',
+		ComponentToRender: Contact
+	}
+];
+
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+	<div>
+		<Navigation />
+		{routeDetails.map(({ id, path, ComponentToRender }) => {
+			return <Route key={id} exact path={path} component={ComponentToRender} />;
+		})}
+	</div>
 );
 
 export default App;
